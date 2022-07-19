9ab8e0064294a22d0219b8a888702aa98b6b5716
wamos <kelvinstwang@gmail.com>
2022-07-19 14:11:12 -0700

    use Eigen tensor instead of matrix for batches, and add more iterations so VTune can detect it

--- mel_scale.cc
+++ mel_scale.cc
@@ -2,27 +2,43 @@
 #include <iostream>
 #include <cmath>
 #include <Eigen/Dense>
+#include <unsupported/Eigen/CXX11/Tensor>
  
 typedef Eigen::Matrix<int8_t, Eigen::Dynamic, Eigen::Dynamic> Matrix_int8;
  
 int main() {
-	// input data is 384000 audio samples of float
-	// take a FFT: window size 2048, hop 512 samples for each window
-	// yes the FFT accelerator aka Xilinx HLS FFT takes float
-	Eigen::MatrixXf FFTOuput(1025, 750);
-	auto FFTOuput_transposed = FFTOuput.transpose();
-	FFTOuput = FFTOuput*FFTOuput_transposed; // square for power 
-	FFTOuput = FFTOuput/700;
-	//ref: https://stackoverflow.com/questions/33786662/apply-function-to-all-eigen-matrix-element
-	FFTOuput = FFTOuput.unaryExpr([](float x){return x + 1;});
-	FFTOuput = FFTOuput.unaryExpr([](float x){return std::log10(x);});
-	FFTOuput = FFTOuput*2595;
-	// skip some details of how mel-scale with 128 bins maps to FFTouput 	
-	
-	// after MFCC
-	Eigen::MatrixXf MFCCAudio(128, 750);
-	MFCCAudio.conservativeResize(1, 128*750); // reshape to a vector
-	// std::cout << "MFCCAudio is of size " << MFCCAudio.rows() << "x" << MFCCAudio.cols() << std::endl;
-	Matrix_int8 MFCCAudio_int8t = MFCCAudio.cast <int8_t> (); 
-	// std::cout << "audio is of size " << MFCCAudio_int8t.rows() << "x" << MFCCAudio_int8t.cols() << std::endl;
+	uint32_t iterations = 2;//1024*1024;
+	uint32_t batch_size = 2;	
+
+	for(int iter = 0; iter < iterations; iter++){
+		Eigen::Tensor<float, 3> FFTOuput(1025, 750, batch_size);
+		Eigen::Tensor<float, 3> matmul(1025, 1025, batch_size);
+		Eigen::Tensor<float, 3> constmat(1025, 1025, batch_size);	
+		FFTOuput.setRandom();
+
+		Eigen::array<Eigen::Index, 3> extents = {1, 0, 2};
+		//auto FFTOuput_transposed = FFTOuput.transpose();
+		Eigen::Tensor<float, 3> FFTOuput_transposed = FFTOuput.shuffle(extents); 
+		//FFTOuput = FFTOuput*FFTOuput_transposed; // square for power 
+		for(int batch = 0; batch < batch_size; batch++){
+			Eigen::Tensor<float, 2> mat_a = FFTOuput.chip(batch,2);
+			Eigen::Tensor<float, 2> mat_b = FFTOuput_transposed.chip(batch,2);
+			Eigen::Tensor<float, 2> mat_ab = matmul.chip(batch,2);
+			// std::cout << "mat_a:\n" << mat_a << std::endl;
+			// std::cout << "mat_b:\n" << mat_b << std::endl;
+			Eigen::array<Eigen::IndexPair<int>, 1> product_dims = { Eigen::IndexPair<int>(1, 0) };
+			mat_ab = mat_a.contract(mat_b, product_dims);
+			std::cout << mat_ab.NumDimensions << "\n";
+		}
+		constmat.setConstant(700);
+		matmul = matmul/constmat;
+		matmul = matmul.unaryExpr([](float x){return x + 1;});
+		matmul = matmul.unaryExpr([](float x){return std::log10(x);});
+		constmat.setConstant(2595);
+		matmul = matmul*constmat;
+		Eigen::Tensor<int8_t, 3>  MFCCAudio_int8t = matmul.cast <int8_t> (); 	
+		Eigen::array<uint32_t, 3> three_dims{1025*1025, 1, batch_size};
+		MFCCAudio_int8t = MFCCAudio_int8t.reshape(three_dims);
+		std::cout << MFCCAudio_int8t.NumDimensions << "\n";
+	}
 }
--- reshape_casting.cc
+++ reshape_casting.cc
@@ -2,14 +2,40 @@
 #include <iostream>
 #include <cmath>
 #include <Eigen/Dense>
+#include <unsupported/Eigen/CXX11/Tensor>
  
 typedef Eigen::Matrix<int8_t, Eigen::Dynamic, Eigen::Dynamic> Matrix_int8;
+using Eigen::Tensor;
  
 int main() {
-	Eigen::MatrixXf FFTOuput(16, 250);
-	auto FFTOuput_transposed = FFTOuput.transpose();
-	FFTOuput = FFTOuput*FFTOuput_transposed; // square for power 
-	FFTOuput.conservativeResize(1, 16*250); // reshape to a vector
-	Matrix_int8 FFTOuput_int8t = FFTOuput.cast <int8_t> (); 
-	std::cout << "audio is of size " << FFTOuput_int8t.rows() << "x" << FFTOuput_int8t.cols() << std::endl;
+	uint32_t iterations = 2;//1024*1024;
+	uint32_t batch_size = 1024;
+
+	for(int iter = 0; iter < iterations; iter++){		
+		Eigen::Tensor<float, 3> input(16, 250, batch_size);
+		Eigen::Tensor<float, 3> const2(16, 250, batch_size);
+		Eigen::Tensor<float, 3> matmul(250, 250, batch_size);
+		input.setConstant(2);
+		const2.setConstant(0.5);
+		Eigen::array<Eigen::Index, 3> extents = {1, 0, 2};
+		input = input*const2;
+		Eigen::Tensor<float, 3> output = input.shuffle(extents); // transpose of tensor along dim 0 and 1
+		//std::cout << "dims:" << output.dimension(0) << "," << output.dimension(1) << "," << output.dimension(2) << std::endl;
+		//output.setConstant(5);
+
+		for(int batch = 0; batch < batch_size; batch++){
+			Eigen::Tensor<float, 2> mat_a = input.chip(batch,2);
+			Eigen::Tensor<float, 2> mat_b = output.chip(batch,2);
+			Eigen::Tensor<float, 2> mat_ab = matmul.chip(batch,2);
+			// std::cout << "mat_a:\n" << mat_a << std::endl;
+			// std::cout << "mat_b:\n" << mat_b << std::endl;
+			Eigen::array<Eigen::IndexPair<int>, 1> product_dims = { Eigen::IndexPair<int>(1, 0) };
+			mat_ab = mat_a.contract(mat_b, product_dims);
+			//std::cout << mat_ab.NumDimensions << "\n";
+		}
+		Eigen::array<uint32_t, 3> three_dims{1, 250*250, batch_size};
+		matmul = matmul.reshape(three_dims);
+		Eigen::Tensor<int8_t, 3> matmul_int8 = matmul.cast<int8_t>();
+		//std::cout << "dims:" << matmul_int8.dimension(0) << "," << matmul_int8.dimension(1) << "," << matmul_int8.dimension(2) << std::endl;
+	}
 }
